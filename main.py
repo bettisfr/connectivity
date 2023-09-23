c1143bc74ee30791322ad800f05b1fc689e8fada
Francesco Betti Sorbelli <francesco.betti.sorbelli@gmail.com>
2023-09-23 16:40:53 +0200

    update users

--- main.py
+++ main.py
@@ -6,36 +6,48 @@ import folium
 # Create a map object with custom settings
 m = folium.Map(location=[43.041169, 12.560277], zoom_start=12)
 
-# Iterate over the "bike" and "car" folders
-for folder_name in ["bike", "car"]:
-    # Iterate over the CSV files in the subfolder
-    for file_path in glob.glob(f'dataset/{folder_name}/signal-*.csv'):
-        # Extract the date from the file name
-        file_name = os.path.basename(file_path)
-        date = file_name.split('-')[1:]
-        formatted_date = '-'.join(date).split('.')[0]
+# Define colors for each vehicle type
+vehicle_colors = {
+    "car": 'red',
+    "bike": 'blue',
+    "walk": '#00FF00',
+    "train": 'magenta'
+}
 
-        # Read the CSV file
-        with open(file_path, 'r') as file:
-            reader = csv.reader(file)
-            next(reader)  # Skip the header row if it exists
+# Iterate over user folders
+for user_folder in glob.glob('dataset/*'):
+    user_id = os.path.basename(user_folder)  # Extract user ID from folder name
 
-            # Create a list to store the trail coordinates
-            trail_coordinates = []
+    # Iterate over the vehicle types (car, bike, walk, train)
+    for vehicle_type in ["car", "bike", "walk", "train"]:
+        # Iterate over the CSV files in the subfolder
+        for file_path in glob.glob(f'{user_folder}/{vehicle_type}/signal-*.csv'):
+            # Extract the date from the file name
+            file_name = os.path.basename(file_path)
+            date = file_name.split('-')[1:]
+            formatted_date = '-'.join(date).split('.')[0]
 
-            # Iterate over the rows and extract latitudes and longitudes
-            for row in reader:
-                latitude, longitude = float(row[0]), float(row[1])
-                trail_coordinates.append((latitude, longitude))
+            # Read the CSV file
+            with open(file_path, 'r') as file:
+                reader = csv.reader(file)
+                next(reader)  # Skip the header row if it exists
 
-            # Determine the color based on the folder name
-            color = 'blue' if folder_name == 'bike' else 'red'
+                # Create a list to store the trail coordinates
+                trail_coordinates = []
 
-            # Create the tooltip text
-            tooltip = f'{folder_name} {formatted_date}'
+                # Iterate over the rows and extract latitudes and longitudes
+                for row in reader:
+                    latitude, longitude = float(row[0]), float(row[1])
+                    trail_coordinates.append((latitude, longitude))
 
-            # Add a polyline to the map with the trail coordinates, color, and tooltip
-            folium.PolyLine(trail_coordinates, color=color, tooltip=tooltip).add_to(m)
+                # Get the color based on the vehicle type
+                color = vehicle_colors.get(vehicle_type, 'gray')  # Default to gray if type is unknown
+
+                # Create the tooltip text
+                tooltip = f'{user_id} - {vehicle_type} - {formatted_date}'
+
+                # Add a polyline to the map with the trail coordinates, color, and tooltip
+                folium.PolyLine(trail_coordinates, color=color, tooltip=tooltip).add_to(m)
 
 # Save the map to an HTML file
 m.save('map.html')
